5aaf4e02f0d8922b178a670c1d9972d55295552d
SaidGM <said.mg.1517@gmail.com>
2023-02-27 11:57:57 +0100

    'tiposDeDatos'

--- src/com/company/ByeWorld.java
+++ src/com/company/ByeWorld.java
@@ -1,2 +1,7 @@
-package com.company;public class ByeWorld {
+package com.company;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.println("Adios mundo");
+    }
 }
--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -1,2 +1,36 @@
-package com.company;public class HelloWorld {
+package com.company;
+
+public class HelloWorld {
+    public static void main(String[] args){
+
+        //enteros
+
+        byte number1 = 1;
+        short number2 = 2;
+        int number3= 3;
+        long number4 = 4;
+
+        //punto flotante
+
+        float decimal1 = 4.9f;
+        double decimal2 = 1.99;
+
+        //caracter
+
+        char caracter1 = 'b';
+
+        //boleanos
+
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //cadenas de texto
+
+        String nombre = "Alan";
+        String apellido = "Garcia";
+
+        //tipo envoltorio
+
+        Integer numero = null;
+    }
 }
